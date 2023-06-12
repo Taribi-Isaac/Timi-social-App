86bc236ace6749574d07515e357f0a038cd0ae9b
Taribi-Isaac <taribiforoyou@gmail.com>
2023-06-12 10:36:55 +0100

    fourth commit

--- socket/index.js
+++ socket/index.js
@@ -13,15 +13,35 @@ const addUser = (userId , socketId)=>{
 
 const removeUser = (socketId)=>{
   users = users.filter((user)=> user.socketId !== socketId)
+};
+
+const getUser = (userId)=>{
+  return users.find(user=>user.userId === userId);
+  
 }
 
 io.on("connection", (socket)=>{
+
+  //when connected
   console.log("a user connected")
+
+  //take useid and socketId from user
   socket.on("addUser", (userId)=>{
 
     addUser(userId, socket.id)
     io.emit("getUsers", users);
   });
+
+  //send and get message
+  socket.on("sendMessage", ({senderId, receiverId, text})=>{
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId, text,
+    })
+    
+  })
+
+  //when disconnected
   socket.on("disconnect", ()=>{
     console.log("a user disconnected")
     removeUser(socket.id)
